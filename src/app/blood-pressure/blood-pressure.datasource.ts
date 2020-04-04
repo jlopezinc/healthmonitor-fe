37b7ba5cd71ea90b8b3744d730b92a49d58288f3
Joao Lopes <jlopes@tillster.com>
2020-04-04 20:24:30 +0100

    Added pagination: missing file

---
+++ src/app/blood-pressure/blood-pressure.datasource.ts
@@ -0,0 +1,39 @@
+
+import {CollectionViewer, DataSource} from "@angular/cdk/collections";
+import {Observable, BehaviorSubject, of} from "rxjs";
+import {catchError, finalize} from "rxjs/operators";
+import { BloodPressure, BloodPressureService } from './blood-pressure.service';
+
+export class BloodPressureDatasource implements DataSource<BloodPressure> {
+
+    private bloodPressureSubject = new BehaviorSubject<BloodPressure[]>([]);
+    private loadingSubject = new BehaviorSubject<boolean>(false);
+
+    public loading$ = this.loadingSubject.asObservable();
+
+    constructor(private bloodPressureService: BloodPressureService) {
+
+    }
+
+    loadBloodPressures(pageIndex:number, pageSize:number) {
+
+        this.loadingSubject.next(true);
+
+        this.bloodPressureService.getBloodPressure(pageIndex, pageSize).pipe(
+                catchError(() => of([])),
+                finalize(() => this.loadingSubject.next(false))
+            )
+            .subscribe(bp => this.bloodPressureSubject.next(bp));
+
+    }
+
+    connect(collectionViewer: CollectionViewer): Observable<BloodPressure[]> {
+        console.log("Connecting data source");
+        return this.bloodPressureSubject.asObservable();
+    }
+
+    disconnect(collectionViewer: CollectionViewer): void {
+        this.bloodPressureSubject.complete();
+        this.loadingSubject.complete();
+    }
+}
