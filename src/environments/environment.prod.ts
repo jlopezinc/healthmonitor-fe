899a0bbb90f04767e79ce1653193e973a0beed8b
Joao Lopes <jlopes@tillster.com>
2024-02-15 14:49:57 +0000

    moving Auth0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     domain: 'dev-nt5fx60p.eu.auth0.com',
     clientId: 'hRkhry1HfgpopTDom0KdcXjVxYHea6AK',
     redirectUri: window.location.origin,
-    audience: 'https://healthmonitor.jlopezinc.com'
+    audience: 'https://dev-nt5fx60p.eu.auth0.com/api/v2/'
   },
   serverUrl: 'https://healthmonitor-be.jlopezinc.com'
 };
